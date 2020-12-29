82941648dd526164151c0cb642cd435244158c1d
alkn203 <alkn203@gmail.com>
2020-12-30 02:01:38 +0900

    up

---
+++ main.js
@@ -0,0 +1,72 @@
+// グローバルに展開
+phina.globalize();
+// アセット
+var ASSETS = {
+  // 画像
+  image: {
+    'tomapiko': 'assets/images/tomapiko_ss.png',
+    //'tiles': 'assets/images/tiles.png',
+  },
+  // フレームアニメーション情報
+  spritesheet: {
+    'tomapiko_ss': 'https://cdn.jsdelivr.net/gh/phinajs/phina.js@develop/assets/tmss/tomapiko.tmss',
+  },
+};
+/*
+ * メインシーン
+ */
+phina.define("MainScene", {
+  // 継承
+  superClass: 'DisplayScene',
+  // 初期化
+  init: function() {
+    // 親クラス初期化
+    this.superInit();
+    // 背景色
+    this.backgroundColor = 'black';
+    // アセットマネージャー
+    var manager = phina.asset.AssetManager;
+    // グループ
+    var imageGroup = DisplayElement().addChildTo(this);
+    // 画像アセット名の配列
+    var images = Object.keys(manager.assets.image);
+    // ドロップダウンリストを取得
+    var selector = document.querySelector('#image_selector');
+    // 選択肢を作成
+    images.each(function(name) {
+      var op = document.createElement('option');
+      op.text = name;
+      selector.appendChild(op);
+    });
+    // 一番目の画像を表示
+    var sp = Sprite(images.first).addChildTo(imageGroup);
+    sp.setPosition(this.gridX.center(), this.gridY.center());
+    var self = this;
+    // 選択肢変更イベント
+    selector.addEventListener('change', function(event) {
+      imageGroup.children.clear();
+      var sp = Sprite(event.target.value).addChildTo(imageGroup);
+      sp.setPosition(self.gridX.center(), self.gridY.center());
+    });
+  },
+});
+/*
+ * メイン処理
+ */
+phina.main(function() {
+  // アプリケーションを生成
+  var app = GameApp({
+    // 表示先のcanvasを指定
+    query: '#mycanvas',
+    // MainScene から開始
+    startLabel: 'main',
+    // 画面にフィットさせない
+    fit: false,
+    // アセット読み込み
+    assets: ASSETS,
+  });
+  // fps表示
+  //app.enableStats();
+  // 実行
+  app.run();
+});
